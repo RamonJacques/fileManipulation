5a96bd7964f9ca8156cbd81fd495f18be5be2d49
RamonJacques <ramonjacquesrs@gmail.com>
2023-06-18 19:15:24 -0300

    File reader + buffered reader implemented

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,23 +1,27 @@
 package application;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Locale;
-import java.util.Scanner;
 
 public class Program {
 
 	public static void main(String[] args) {
 
 		Locale.setDefault(Locale.US);
-		Scanner sc = new Scanner(System.in);
-		
-		
-		
-		
-		
-		
-		
-		
-		sc.close();
+
+		try (BufferedReader br = new BufferedReader(
+				new FileReader("c:\\temp\\ws-eclipse\\fileManipulation\\item-list.csv"))) {
+			String line = br.readLine();
+
+			while (line != null) {
+				System.out.println(line);
+				line = br.readLine();
+			}
+		} catch (IOException e) {
+			System.out.println("Error: " + e.getMessage());
+		}
 	}
 
 }
